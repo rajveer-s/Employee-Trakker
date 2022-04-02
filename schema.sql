d9da14bae093711d6a8f1d614f182d557d453c7d
Rajveer Singh <rajsidhu2924@gmail.com>
2022-04-01 20:04:09 -0700

    employee table created

--- schema.sql
+++ schema.sql
@@ -21,3 +21,19 @@ CREATE TABLE role (
   REFERENCES department(id)
   ON DELETE SET NULL
 );
+
+-- Employye table  
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) 
+  REFERENCES role(id) 
+  ON DELETE SET NULL,
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
+);
