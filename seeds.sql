9598b3749570a0eaec765126453d718391dd725d
Rajveer Singh <rajsidhu2924@gmail.com>
2022-04-01 20:06:54 -0700

    employee seeds created

--- seeds.sql
+++ seeds.sql
@@ -1,5 +1,4 @@
 -- Adding data to department table
-
 INSERT INTO department (name)
 VALUES ("IT"),
        ("Finance"),
@@ -7,7 +6,6 @@ VALUES ("IT"),
        ("Marketing");
 
 -- Adding data to Role table
-
        INSERT INTO role (title, salary, department_id)
 VALUES ("IT Director", 300000, 1),
        ("Engineer", 150000, 1),
@@ -18,3 +16,14 @@ VALUES ("IT Director", 300000, 1),
        ("Distribution", 60000, 4);
 
 -- Adding data to Employee table
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Geraldine", "Chance", 1, NULL),
+       ("Bob", "Smith", 1, 1),
+       ("Trisha", "Way", 2, 1),
+       ("Corynn", "Tracey", 2, 1),
+       ("Ronald", "Perkins", 3, NULL),
+       ("Tony", "Stark", 3, 5),
+       ("Barnaby", "Arthurson", 4, NULL),
+       ("Travers", "Wood", 5, 7),
+       ("Jeffery", "Freeman", 6, 7),
+       ("Steve", "Rodgers", 7, 7);
