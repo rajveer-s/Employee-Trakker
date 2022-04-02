cc01c03b15708e375a4b9fbb133f1a9d40ff0651
Rajveer Singh <rajsidhu2924@gmail.com>
2022-04-01 20:11:57 -0700

    inquirer with choices// promptMenu function created

--- index.js
+++ index.js
@@ -31,3 +31,55 @@ const db = mysql.createConnection(
 );
 // brings up the questions right whem the database is fired 
 promtMenu()
+
+// this function will display a list of choices for the user to select from
+function promtMenu() {
+  inquirer.prompt([{
+    type: 'list',
+    name: 'menu',
+    message: 'What would you like to do?',
+    choices: [
+      'View All Employees',
+      'Add Employee',
+      'Update Employee Role',
+      'View All Roles',
+      'Add Role',
+      'View All Departments',
+      'Add Department',
+      'I am all done'
+    ]
+  }
+  ]).then(answers => {
+    switch (answers.menu) {
+      case "View All Employees":
+        viewAllEmp()  //view all employees function 
+        break;
+      case "Add Employee":
+        addEmployee() // run Add employee function
+        break;
+
+      case "Update Employee Role":
+        //run update employee role function 
+        break;
+
+      case "View All Roles":
+        viewAllRoles() // view all roles function
+        break;
+
+      case "Add Role":
+        // add role function
+        break;
+
+      case "View All Departments":
+        viewAllDept(); // view all departments function
+        break;
+
+      case "Add Department":
+        addDepartment(); // add department function
+        break;
+
+      default:
+        process.exit();
+    }
+  });
+}
