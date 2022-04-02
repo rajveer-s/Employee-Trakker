63f84233fb33295bd3cac69cfea8cf6d13b56e59
Rajveer Singh <rajsidhu2924@gmail.com>
2022-04-01 20:06:13 -0700

    role seeds created

--- seeds.sql
+++ seeds.sql
@@ -1,5 +1,20 @@
+-- Adding data to department table
+
 INSERT INTO department (name)
 VALUES ("IT"),
        ("Finance"),
        ("Sales"),
        ("Marketing");
+
+-- Adding data to Role table
+
+       INSERT INTO role (title, salary, department_id)
+VALUES ("IT Director", 300000, 1),
+       ("Engineer", 150000, 1),
+       ("Accounting", 140000, 2),
+       ("The Sales Manager", 180000, 3),
+       ("Sales Development Rep", 80000, 3),
+       ("Digital Marketing", 75000, 4),
+       ("Distribution", 60000, 4);
+
+-- Adding data to Employee table
