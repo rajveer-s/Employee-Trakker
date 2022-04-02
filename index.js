c67c3bb52027a3bbb0b340c839c893299acc0cd0
Rajveer Singh <rajsidhu2924@gmail.com>
2022-04-01 20:10:35 -0700

    database connect funcion added

--- index.js
+++ index.js
@@ -3,3 +3,31 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 require("console.table");
+
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // Add MySQL password here
+    password: "password",
+    database: "company_db",
+  },
+  console.log(
+    `
+  
+          -----------------------------------------
+         |                                        |
+         |           WELCOME, You are now         | 
+         |                                        |
+         |    connected to the company database   |  
+         |                                        |      
+          -----------------------------------------
+
+
+`)
+);
+// brings up the questions right whem the database is fired 
+promtMenu()
