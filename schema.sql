3180e2302158ec3b6e7c9be81202a7fbbac2cad5
Rajveer Singh <rajsidhu2924@gmail.com>
2022-04-01 20:03:32 -0700

    role table created

--- schema.sql
+++ schema.sql
@@ -4,7 +4,20 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
+-- department table 
 CREATE TABLE department (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
+);
+
+-- Role table 
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT PRIMARY KEY,  
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
 );
